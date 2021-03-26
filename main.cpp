4fdecbc2341a54c8d90a1be09e4bacdaf7e16745
Valeria Cabrera <valeriajanine1@gmail.com>
2021-03-26 16:13:32 -0600

    Creo que ya funciona el convertirTokens

--- main.cpp
+++ main.cpp
@@ -33,8 +33,51 @@ vector<string> tokenizer(string linea) {
 		linea.erase(0,pos + delim.length());
 	}
 	result.push_back(linea);
-	
+
 	return result;
+}
+
+vector<Dato> convertirTokens(vector<string> tokens) {
+	vector<Dato> data;
+
+	for(int i = 0; i < tokens.size(); i++) {
+
+		char aux;
+		string token = tokens.at(i);
+		aux = token[0];
+		Dato d;
+		char tipo;
+		
+		if(i==0){
+			d.c_tipo=aux;
+			aux=tipo;
+		}
+		
+		if(aux=='+'||aux=='-'||aux=='*'||aux=='/'){
+			//es un operador
+			DatoValor dv;
+			dv.operador=aux;
+			d.valor=dv;
+		}else{
+			if(tipo=='I'||tipo=='i'){
+				DatoValor dv;
+				dv.i_dato=(int)aux;
+				d.valor=dv;
+			}else if(tipo=='F'||tipo=='f'){
+				DatoValor dv;
+				dv.i_dato=(float)aux;
+				d.valor=dv;
+			}else if(tipo=='B'||tipo=='b'){
+				DatoValor dv;
+				dv.i_dato=(unsigned int)aux;
+				d.valor=dv;
+			}
+		}
+
+		data.push_back(d);
+	}
+
+	return data;
 }
 
 int main(int argc, char** argv) {
@@ -43,16 +86,23 @@ int main(int argc, char** argv) {
 	string linea="";
 
 	while(getline(leer_texto,linea)) {
-		
+
+
+		//imprimir los tokens
 		vector<string> line;
 		line=tokenizer(linea);
-		for(int i = 0; i < line.size(); i++){
-			cout<<"token "<<i<<" : "<<line.at(i)<<endl;
+		vector<Dato> datos;
+		datos =	convertirTokens(line);
+		for(int i = 0; i < datos.size(); i++) {
+			Dato d;
+			d = datos.at(i);
+			cout<<d.c_tipo<<endl;
 		}
-		
+
 		cout<<"--------------------------"<<endl;
+
 	}
-	
+
 	leer_texto.close();
 	return 0;
 }
